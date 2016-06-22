18ff2e5b8c6421641d69e7324993d3f483e81cff
Fabio Picheli <contato@fabiopicheli.com>
2016-06-22 17:25:50 +0200

    Update background.js

--- background.js
+++ background.js
@@ -6,10 +6,7 @@ $(document).ready(function() {
 });
 
 var cenafy = function() {
-	$('body').append('<div style="width:100%;height:100%;top:0;left:0;background-color:#000;position: fixed;z-index: 999999999;opacity:0;" id="itsJohnCena"></div>');
-	$('#itsJohnCena').animate({
-		opacity: 1,
-	}, 2000, function() {
+	setTimeout(function(){
 		window.location.replace('http://instantcena.com/');
-	});
+	}, 5000);
 };
