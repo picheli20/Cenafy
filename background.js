ab2ef252562acd7b03151715d42f563266380492
Fabio Picheli <contato@fabiopicheli.com>
2016-06-22 15:09:27 +0200

    easier to appers

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	var chance = Math.floor(Math.random() * 100);
-	if(chance == 69) {
+	var chance = Math.floor(Math.random() * 20);
+	if(chance == 1 || chance == 7) {
 		cenafy();
 	}
 });
