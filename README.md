0b1d298323d419c11cdff2bf9a75ae34de22f0a3
Fabio Picheli <contato@fabiopicheli.com>
2016-06-22 17:03:21 +0200

    Redirect and README update

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 A Google Chrome Extension has a 1/10 chance of injecting John Cena on every new page load.
 
+[Download](https://chrome.google.com/webstore/detail/cdilnjpehcjhengliidigkaoklikdkbb/)
+
+
+
 Original (1/100)
+
 [Download](https://chrome.google.com/webstore/detail/cenafy/ndchmakhfaakbkhnkdgambadneloplnn/)
 
 Video sourced from [Instant Cena](http://instantcena.com) Thanks Tony! :D
--- background.js
+++ background.js
@@ -5,21 +5,11 @@ $(document).ready(function() {
 	}
 });
 
-var generateHTML = function() {
-	var html = "<video autoplay loop style='height: 100vh; width: 100vw'><source src='https://raw.githubusercontent.com/Roystbeef/picheli20/master/cena.webm' type='video/webm'><source src='https://raw.githubusercontent.com/Roystbeef/picheli20/master/cena.mp4' type='video/mp4'></video>";
-	return html;
-};
-
 var cenafy = function() {
 	$('body').append('<div style="width:100%;height:100%;top:0;left:0;background-color:#000;position: fixed;z-index: 999999999;opacity:0;" id="itsJohnCena"></div>');
 	$('#itsJohnCena').animate({
 		opacity: 1,
-	}, 5000, function() {
-		var html = generateHTML();
-		$('body').html(html);
-		document.body.style.backgroundColor = "black";
-		setTimeout(function() { // Wait 20 seconds for cena to finish
-			window.location.replace(document.URL);
-		}, 20000);
+	}, 2000, function() {
+		window.location.replace('http://instantcena.com/');
 	});
 };
